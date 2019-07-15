bd3d0111ffb69f008fd00c825dae2eb700ef08e4
abhi.sree <abhi.sree762@gmail.com>
2019-07-15 10:18:15 +0530

    Assignment 3 task 3 pending

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -18,6 +18,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
+
 
 
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -65,7 +67,8 @@ import { LoginComponent } from './login/login.component';
     MatSelectModule,
      MatSlideToggleModule,
      ReactiveFormsModule,
-     MatProgressSpinnerModule
+     MatProgressSpinnerModule,
+     MatSliderModule
   ],
   providers: [
     DishService,
--- conFusion/src/app/dishdetails/dishdetails.component.ts
+++ conFusion/src/app/dishdetails/dishdetails.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from "../shared/comment";
+import { DISHES } from "../shared/dishes";
 
 
 
@@ -14,13 +17,42 @@ import { Location } from '@angular/common';
 })
 export class DishdetailsComponent implements OnInit {
   dish: Dish;
+  comments: Comment;
+
   dishIds: string[];
  prev: string;
  next: string;
+ commentSection: FormGroup;
+ formErrors = {
+ 'author': '',
+ 'rating': '',
+ 'comment': ''
+ };
+
+ validationMessages = {
+   'author': {
+     'required': 'Author Name is required.',
+     'minlength': 'Author Name must be at least 2 characters long.',
+     'maxlength': 'Author tName cannot be more than 25 characters long.'
+   },
+   'rating': {
+     'required': 'rating is required.'
+   },
+   'comment': {
+     'required': 'Comment is required.',
+     'minlength': 'Comment must be at least 5 characters long.',
+     'maxlength': 'Comment cannot be more than 25 characters long.'
+   }
+ };
+
+  @ViewChild('fform') commentSectionDirective;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+     private fb: FormBuilder) {
+     this.createForm();
+    }
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
@@ -29,6 +61,48 @@ export class DishdetailsComponent implements OnInit {
       this.setPrevNext(dish.id);
     });
   }
+  createForm() {
+    this.commentSection = this.fb.group({
+      author: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      rating: ['',[Validators.required, Validators.pattern]],
+      comment: ['',[Validators.required, Validators.minLength(5), Validators.maxLength(25)]]
+    });
+    this.commentSection.valueChanges
+    .subscribe(data => this.onValueChanged(data));
+    this.onValueChanged();
+  }
+
+  onSubmit() {
+    this.comments = this.commentSection.value;
+    console.log(this.comments);
+    this.commentSection.reset({
+      author: '',
+      rating: '',
+      comment: ''
+    });
+    this.commentSectionDirective.resetForm();
+
+  }
+
+  onValueChanged(data?: any) {
+      if (!this.commentSection) { return; }
+      const form = this.commentSection;
+      for (const field in this.formErrors) {
+        if (this.formErrors.hasOwnProperty(field)) {
+          // clear previous error message (if any)
+          this.formErrors[field] = '';
+          const control = form.get(field);
+          if (control && control.dirty && !control.valid) {
+            const messages = this.validationMessages[field];
+            for (const key in control.errors) {
+              if (control.errors.hasOwnProperty(key)) {
+                this.formErrors[field] += messages[key] + ' ';
+              }
+            }
+          }
+        }
+      }
+    }
 
   setPrevNext(dishId: string) {
     const index = this.dishIds.indexOf(dishId);
@@ -36,7 +110,7 @@ export class DishdetailsComponent implements OnInit {
     this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
-  goBack(): void {
+  goBack(): void{
     this.location.back();
   }
 
